0ed4449a89682c714d9500329e50d29574c5f555
Mustafa <Mustafa.Ahmad@metropolia.fi>
2026-06-27 15:03:50 +0300

    Register and login tested

--- synk-web/Backend/src/main/java/com/synk/backend/controller/AuthController.java
+++ synk-web/Backend/src/main/java/com/synk/backend/controller/AuthController.java
@@ -3,16 +3,12 @@ package com.synk.backend.controller;
 
 import com.synk.backend.dto.userDto.UserLoginRequestDto;
 import com.synk.backend.dto.userDto.UserLoginResponseDto;
-import com.synk.backend.repository.UserRepository;
-import com.synk.backend.security.JwtUtil;
+import com.synk.backend.dto.userDto.UserRegisterRequestDto;
+import com.synk.backend.dto.userDto.UserResponseDto;
 import com.synk.backend.service.UserServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +26,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<UserLoginResponseDto> login(@Valid @RequestBody UserLoginRequestDto body) {
         return ResponseEntity.ok(userServiceImpl.login(body));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> login(@Valid @RequestBody UserRegisterRequestDto body) {
+        return ResponseEntity.ok(userServiceImpl.createUser(body));
     }
 
 }
